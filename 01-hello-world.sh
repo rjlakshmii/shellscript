bc190a6d3bbda59a9e82f06b4f96e2feb94f120d
rjlakshmii <rjlakshmi157@gmail.com>
2024-05-24 15:15:33 +0530

    hellow world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo"Hello World, I am learning devops with AWS"
+PERSON1:Ramesh
+PERSON2:Suresh
 
 
-
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Mornin Ramesh"
-echo "Ramesh: How are you Ramesh"
-echo "Suresh: I am Good,How are you Ramesh"
+echo "$PERSION1: Hi $PERSION2, Good Morning"
+echo "$PERSION2: Good Mornin $PERSION1"
+echo "$PERSION1: How are you $PERSION2"
+echo "$PERSION2: I am Good,How are you $PERSION1"
